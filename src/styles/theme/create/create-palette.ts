a9196d9219b9900b0fe5e5fe54ab4eea15137281
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-09-03 00:05:12 +0100

    add createPalette

--- src/styles/theme/create/create-palette.ts
+++ src/styles/theme/create/create-palette.ts
@@ -1,7 +1,91 @@
-import { CreateThemePaletteOptions, ThemePalette } from './create'
+import { blue, common, green, purple, red, yellow } from '../../colors'
+import { createColor, getContrastRatio, ThemeType } from '../../colors/color'
+import {
+    CreateThemePaletteOptions,
+    CreateThemePaletteOptionsColorType,
+    ThemePalette,
+    ThemePaletteColor,
+} from './create'
+
+type CreateIndividualPaletteOptions = {
+    contrastThreshold: number
+    themeType: ThemeType
+}
+
+const createIndividualPalette = (
+    color: CreateThemePaletteOptionsColorType,
+    options: CreateIndividualPaletteOptions
+): ThemePaletteColor => {
+    if (typeof color === 'function') {
+        return color()
+    }
+
+    const { contrastThreshold } = options
+
+    const basicColor =
+        typeof color === 'string'
+            ? createColor(color, options)
+            : createColor(color.baseColor, color)
+
+    const text =
+        getContrastRatio(basicColor[50], common.white) > contrastThreshold
+            ? common.white
+            : common.black
+
+    return {
+        ...basicColor,
+        main: basicColor[50],
+        mainDarkShade: basicColor[60],
+        mainLightShade: basicColor[40],
+        text,
+    }
+}
 
 export const createPalette = (
     options = {} as CreateThemePaletteOptions
 ): ThemePalette => {
-    return {} as any
+    const {
+        primary: primaryProps = green[50],
+        secondary: secondaryProps = purple[50],
+        error: errorProps = red[50],
+        warning: warningProps = yellow[50],
+        info: infoProps = blue[50],
+        contrastThreshold = 3,
+        themeType = 'light',
+    } = options
+
+    const primary = createIndividualPalette(primaryProps, {
+        contrastThreshold,
+        themeType,
+    })
+
+    const secondary = createIndividualPalette(secondaryProps, {
+        contrastThreshold,
+        themeType,
+    })
+
+    const error = createIndividualPalette(errorProps, {
+        contrastThreshold,
+        themeType,
+    })
+
+    const warning = createIndividualPalette(warningProps, {
+        contrastThreshold,
+        themeType,
+    })
+
+    const info = createIndividualPalette(infoProps, {
+        contrastThreshold,
+        themeType,
+    })
+
+    return {
+        primary,
+        error,
+        info,
+        secondary,
+        warning,
+        common,
+        contrastThreshold,
+    }
 }
