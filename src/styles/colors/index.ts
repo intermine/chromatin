e82771accac16d5c4052a003a5784a44ded3eaae
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-09-02 18:44:36 +0100

    refactor(colors): move files

---
+++ src/styles/colors/index.ts
@@ -0,0 +1,18 @@
+export { isColorOrRGBA, isValidColorHex } from './check'
+export {
+    createColor,
+    getContrastRatio,
+    getLuminous,
+    getTintOrShade,
+} from './color'
+export { hex2rgbArray, hex2rgba, RGBA } from './convert'
+
+export { blue } from './constants/blue'
+export { common } from './constants/common'
+export { darkGrey } from './constants/dark-grey'
+export { green } from './constants/green'
+export { grey } from './constants/grey'
+export { red } from './constants/red'
+export { yellow } from './constants/yellow'
+
+export type { BasicColor, CreateColorOptions } from './color'
