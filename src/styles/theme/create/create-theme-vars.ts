befbac4d70f4398a4f4af9d983cb730267ea4f1c
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-09-07 15:18:47 +0100

    update types and add buttonBase

--- src/styles/theme/create/create-theme-vars.ts
+++ src/styles/theme/create/create-theme-vars.ts
@@ -1,15 +1,19 @@
-import { CSSProperties } from 'react'
+import { CSSObject } from 'styled-components'
 
 import type { CreateThemeVarsOptions, ThemeVars } from './create'
 
-const themeVarDefaultFunction = (): CSSProperties => ({})
+const themeVarDefaultFunction = (): CSSObject => ({})
 
 export const createThemeVars = (
     options = {} as CreateThemeVarsOptions
 ): ThemeVars => {
-    const { button = themeVarDefaultFunction } = options
+    const {
+        button = themeVarDefaultFunction,
+        buttonBase = themeVarDefaultFunction,
+    } = options
 
     return {
         button,
+        buttonBase,
     }
 }
