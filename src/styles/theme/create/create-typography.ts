20d0f3146be08c3343476032e346bb77578e285b
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-09-09 15:04:26 +0100

    add createTypography

--- src/styles/theme/create/create-typography.ts
+++ src/styles/theme/create/create-typography.ts
@@ -1,7 +1,132 @@
+import { isProdEnv, mergeDeep } from '../../../utils/misc'
+import {
+    namedTypographyScales as scales,
+    defaultThemeTypography,
+    defaultBoldFontWeight,
+    defaultFontFamily,
+    defaultLightFontWeight,
+    defaultLineHeight,
+    defaultRegularFontWeight,
+    fontMixin,
+    getFontSizeInRem,
+} from '../../typography'
 import { CreateThemeTypographyOptions, ThemeTypography } from './create'
 
 export const createTypography = (
     options = {} as CreateThemeTypographyOptions
 ): ThemeTypography => {
-    return {} as any
+    if (typeof options !== 'function' && typeof options !== 'object') {
+        if (!isProdEnv()) {
+            console.error(
+                '[Chromatin - createTypography]: Expecting options as'.concat(
+                    ' an object or a function. Got',
+                    typeof options
+                )
+            )
+        }
+        return defaultThemeTypography
+    }
+
+    if (typeof options === 'function') return options()
+
+    const {
+        documentFontSize = 16,
+        fontFamily: fontFamilyInput = {},
+        scale: scaleInput = 'goldenRatio',
+    } = options
+
+    const scale =
+        typeof scaleInput === 'number'
+            ? scaleInput
+            : scales[scaleInput] ?? scales['goldenRatio']
+
+    const fontFamily = mergeDeep(fontFamilyInput, {
+        bold: {
+            name: defaultFontFamily,
+            weight: defaultBoldFontWeight,
+            lineHeight: defaultLineHeight,
+        },
+        regular: {
+            name: defaultFontFamily,
+            weight: defaultRegularFontWeight,
+            lineHeight: defaultLineHeight,
+        },
+        light: {
+            name: defaultFontFamily,
+            weight: defaultLightFontWeight,
+            lineHeight: defaultLineHeight,
+        },
+    })
+
+    return {
+        h1: fontMixin(
+            fontFamily.bold.name,
+            getFontSizeInRem(scale, 3),
+            fontFamily.bold.weight,
+            fontFamily.bold.lineHeight
+        ),
+        h2: fontMixin(
+            fontFamily.bold.name,
+            getFontSizeInRem(scale, 2),
+            fontFamily.bold.weight,
+            fontFamily.bold.lineHeight
+        ),
+        h3: fontMixin(
+            fontFamily.bold.name,
+            getFontSizeInRem(scale, 1),
+            fontFamily.bold.weight,
+            fontFamily.bold.lineHeight
+        ),
+        h4: fontMixin(
+            fontFamily.bold.name,
+            getFontSizeInRem(scale, 0),
+            fontFamily.bold.weight,
+            fontFamily.bold.lineHeight
+        ),
+        h5: fontMixin(
+            fontFamily.bold.name,
+            getFontSizeInRem(scale, -1),
+            fontFamily.bold.weight,
+            fontFamily.bold.lineHeight
+        ),
+        h6: fontMixin(
+            fontFamily.bold.name,
+            getFontSizeInRem(scale, -2),
+            fontFamily.bold.weight,
+            fontFamily.bold.lineHeight
+        ),
+        body: fontMixin(
+            fontFamily.regular.name,
+            getFontSizeInRem(scale, 0),
+            fontFamily.regular.weight,
+            fontFamily.regular.lineHeight
+        ),
+        bodySm: fontMixin(
+            fontFamily.regular.name,
+            getFontSizeInRem(scale, -1),
+            fontFamily.regular.weight,
+            fontFamily.regular.lineHeight
+        ),
+        title: fontMixin(
+            fontFamily.bold.name,
+            getFontSizeInRem(scale, 0),
+            fontFamily.bold.weight,
+            fontFamily.bold.lineHeight
+        ),
+        caption: fontMixin(
+            fontFamily.bold.name,
+            getFontSizeInRem(scale, -1),
+            fontFamily.bold.weight,
+            fontFamily.bold.lineHeight
+        ),
+        small: fontMixin(
+            fontFamily.light.name,
+            getFontSizeInRem(scale, -2),
+            fontFamily.light.weight,
+            fontFamily.light.lineHeight
+        ),
+        meta: {
+            documentFontSize,
+        },
+    }
 }
