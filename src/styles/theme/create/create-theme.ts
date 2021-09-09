a9aadd8df55d83ac56859994cc818e2a0ee4005e
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-09-09 15:39:43 +0100

    update create theme

--- src/styles/theme/create/create-theme.ts
+++ src/styles/theme/create/create-theme.ts
@@ -1,22 +1,52 @@
-import { CreateThemeOptions, Theme } from './create'
+import { isProdEnv } from '../../../utils/misc'
+import { CreateThemeOptions, Theme, ThemeColorName } from './create'
 import { createPalette } from './create-palette'
 import { createThemeVars } from './create-theme-vars'
 import { createTypography } from './create-typography'
+import { createElevation } from './create-elevation'
 
+export const isThemeColorName = (
+    colorName: string
+): colorName is ThemeColorName => {
+    if (typeof colorName !== 'string') {
+        if (!isProdEnv()) {
+            console.error(
+                '[Chromatin - isThemeColorName]: Expecting '.concat(
+                    'colorName as string. Got: ',
+                    typeof colorName
+                )
+            )
+        }
+        return false
+    }
+
+    if (
+        colorName === 'primary' ||
+        colorName === 'secondary' ||
+        colorName === 'error' ||
+        colorName === 'warning' ||
+        colorName === 'info'
+    )
+        return true
+    return false
+}
 export const createTheme = (options = {} as CreateThemeOptions): Theme => {
     const {
         palette: paletteOptions = {},
         themeVars: themeVarsOptions = {},
         typography: typographyOptions = {},
+        elevation: elevationOptions = {},
     } = options
 
     const themeVars = createThemeVars(themeVarsOptions)
     const palette = createPalette(paletteOptions)
     const typography = createTypography(typographyOptions)
+    const elevation = createElevation(elevationOptions)
 
     return {
         themeVars,
         palette,
         typography,
+        elevation,
     }
 }
