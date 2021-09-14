67d3c5b1a858eaf8c80a293b0021473a9c6a294a
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-09-14 13:09:39 +0100

    add imports and exports

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// -------------------------
+// Components
+// -------------------------
+
+export { Button } from './button'
+export { ButtonBase } from './button-base'
+
+// -------------------------
+// Types
+// -------------------------
+
+export type { ButtonProps } from './button'
+export type { ButtonBaseCommonProps, ButtonBaseProps } from './button-base'
