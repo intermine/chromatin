f6040d5f5e551f6b37738bc3433ddc0833c02de5
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-09-14 17:46:27 +0100

    add button group

---
+++ src/button-group/button-group.stories.tsx
@@ -0,0 +1,112 @@
+import { Story } from '@storybook/react/types-6-0'
+
+import { ButtonGroup } from './button-group'
+import { Button } from '../button'
+import { IconButton } from '../icon-button'
+
+// Icons
+import DeleteIcon from '../icons/System/delete-bin-6-fill'
+import EditIcon from '../icons/Design/edit-fill'
+import FilterIcon from '../icons/System/filter-fill'
+import SettingIcon from '../icons/System/settings-fill'
+
+export default {
+    title: 'ButtonGroup',
+    component: ButtonGroup,
+}
+
+const Template: Story = ({ children, ...args }) => (
+    <ButtonGroup {...args}>
+        <Button
+            className="test"
+            onClick={() => console.log('Button 1 Clicked')}
+        >
+            Button 1
+        </Button>
+        <Button onClick={() => console.log('Button 2 Clicked')}>
+            Button 2
+        </Button>
+        <Button onClick={() => console.log('Button 3 Clicked')}>
+            Button 3
+        </Button>
+        <Button onClick={() => console.log('Button 4 Clicked')}>
+            Button 4
+        </Button>
+    </ButtonGroup>
+)
+
+const Template2: Story = ({ children, ...args }) => (
+    <ButtonGroup {...args}>
+        <IconButton
+            className="test"
+            onClick={() => console.log('Button 1 Clicked')}
+            Icon={<EditIcon />}
+        />
+        <IconButton
+            onClick={() => console.log('Button 2 Clicked')}
+            Icon={<FilterIcon />}
+        />
+        <IconButton
+            onClick={() => console.log('Button 3 Clicked')}
+            Icon={<DeleteIcon />}
+        />
+        <IconButton
+            onClick={() => console.log('Button 4 Clicked')}
+            Icon={<SettingIcon />}
+        />
+    </ButtonGroup>
+)
+
+const Template3: Story = ({ children, ...args }) => (
+    <ButtonGroup {...args}>
+        <Button
+            className="test"
+            onClick={() => console.log('Button 1 Clicked')}
+            LeftIcon={<EditIcon />}
+        >
+            Edit
+        </Button>
+        <Button
+            onClick={() => console.log('Button 2 Clicked')}
+            LeftIcon={<FilterIcon />}
+        >
+            Filter
+        </Button>
+        <Button
+            onClick={() => console.log('Button 3 Clicked')}
+            LeftIcon={<DeleteIcon />}
+        >
+            Delete
+        </Button>
+        <Button
+            onClick={() => console.log('Button 4 Clicked')}
+            LeftIcon={<SettingIcon />}
+        >
+            Settings
+        </Button>
+    </ButtonGroup>
+)
+
+export const Default = Template.bind({})
+Default.args = {
+    color: 'primary',
+    variant: 'normal',
+    Component: 'button',
+    onClick: () => console.log('Main button clicked'),
+}
+
+export const IconButtonGroup = Template2.bind({})
+IconButtonGroup.args = {
+    color: 'primary',
+    variant: 'normal',
+    Component: 'button',
+    onClick: () => console.log('Main button clicked'),
+}
+
+export const ButtonGroupWithButtonHavingIcon = Template3.bind({})
+ButtonGroupWithButtonHavingIcon.args = {
+    color: 'primary',
+    variant: 'normal',
+    Component: 'button',
+    onClick: () => console.log('Main button clicked'),
+}
---
+++ src/button-group/button-group.tsx
@@ -0,0 +1,48 @@
+import React, { HTMLProps } from 'react'
+import { ButtonBaseCommonProps } from '..'
+import { createStyle } from '../styles'
+
+export interface ButtonGroupProps
+    extends Omit<HTMLProps<HTMLDivElement>, 'as' | 'ref' | 'size'> {
+    variant?: ButtonBaseCommonProps['variant']
+    size?: 'small' | 'regular' | 'large'
+    innerRef?: React.RefObject<any>
+}
+
+const ButtonGroupRoot = createStyle<'div', ButtonGroupProps>('div', () => {
+    return {
+        boxSizing: 'border-box',
+        display: 'inline-flex',
+        '& .bg-child': {
+            borderRadius: 0,
+            borderRightWidth: 0,
+            marginRight: '0.0625rem',
+            marginLeft: '0.0625rem',
+        },
+
+        '& .bg-child:first-child': {
+            borderBottomLeftRadius: '0.25rem',
+            borderTopLeftRadius: '0.25rem',
+            marginLeft: 0,
+        },
+        '& .bg-child:last-child': {
+            borderBottomRightRadius: '0.25rem',
+            borderTopRightRadius: '0.25rem',
+            marginRight: 0,
+        },
+    }
+})
+
+export const ButtonGroup = (props: ButtonGroupProps): JSX.Element => {
+    const { children: childrenProps, ...rest } = props
+    const children = React.Children.map(childrenProps, (child: any) => {
+        return React.cloneElement(child, {
+            ...rest,
+            ...child.props,
+            className: 'bg-child ' + (child.props.className ?? ''),
+            elevation: false,
+        })
+    })
+
+    return <ButtonGroupRoot {...rest}>{children}</ButtonGroupRoot>
+}
