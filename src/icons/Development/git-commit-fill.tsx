e73348318bb1c3e381ba8348199be304d4a26cc3
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-09-14 12:58:49 +0100

    feat: add icons

---
+++ src/icons/Development/git-commit-fill.tsx
@@ -0,0 +1,10 @@
+import { IconContainer, IconContainerProps } from '../icon-container'
+
+export default (props: IconContainerProps): JSX.Element =>
+        <IconContainer {...props}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g>
+        <path fill="none" d="M0 0h24v24H0z"/>
+        <path d="M15.874 13a4.002 4.002 0 0 1-7.748 0H3v-2h5.126a4.002 4.002 0 0 1 7.748 0H21v2h-5.126z"/>
+    </g>
+</svg>
+</IconContainer>
