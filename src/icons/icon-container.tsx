c23f390b8c02843a4d112a90ab72b145413db946
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-09-17 16:25:20 +0100

    add default dimension for icon container

--- src/icons/icon-container.tsx
+++ src/icons/icon-container.tsx
@@ -1,17 +1,48 @@
+import { CSSObject } from 'styled-components'
 import { createStyledComponent, ReactElement } from '../styles'
 
 export type IconContainerProps = {
     children?: ReactElement
+    size?: 'small' | 'regular' | 'large'
 }
 
-const Span = createStyledComponent('span', (theme, props) => {
-    return {
-        alignItems: 'center',
-        display: 'inline-flex',
-        justifyContent: 'center',
+const Span = createStyledComponent<'span', IconContainerProps>(
+    'span',
+    (_, props) => {
+        const { size } = props
+
+        const getDimension = (): CSSObject => {
+            if (!size) return {}
+            if (size === 'regular') {
+                return {
+                    height: '1rem',
+                    width: '1rem',
+                }
+            }
+
+            if (size === 'large') {
+                return {
+                    height: '1.5rem',
+                    width: '1.5rem',
+                }
+            }
+
+            return {
+                height: '0.5rem',
+                width: '0.5rem',
+            }
+        }
+
+        return {
+            alignItems: 'center',
+            display: 'inline-flex',
+            justifyContent: 'center',
+            ...getDimension(),
+        }
     }
-})
+)
 
 export const IconContainer = (props: IconContainerProps) => {
-    return <Span>{props.children}</Span>
+    const { children, ...rest } = props
+    return <Span {...rest}>{props.children}</Span>
 }
