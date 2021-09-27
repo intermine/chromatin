836af9e82a0e6f7fdaf41fa434c6d562c78d26f1
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-09-27 17:29:46 +0100

    add fn to confirm anchor element

---
+++ src/utils/validate.ts
@@ -0,0 +1,44 @@
+import { isProdEnv } from './misc'
+
+export const isValidAnchorElement = (
+    anchorElement?: Element
+): anchorElement is Element => {
+    if (anchorElement === null) return false
+
+    if (!anchorElement || typeof anchorElement !== 'object') {
+        if (!isProdEnv()) {
+            console.error(
+                [
+                    '[Chromatin - Popper]: Anchor Element is not defined.',
+                    'Expecting anchorElement as HTML Element, ',
+                    `Got: ${typeof anchorElement}`,
+                ].join(' ')
+            )
+        }
+
+        return false
+    }
+
+    const element = anchorElement.getBoundingClientRect()
+
+    if (
+        process.env.NODE_ENV !== 'test' &&
+        element.top === 0 &&
+        element.left === 0 &&
+        element.right === 0 &&
+        element.bottom === 0
+    ) {
+        if (!isProdEnv()) {
+            console.error(
+                [
+                    '[Chromatin - Popper]: anchorElement is not the',
+                    'part of the layout. Make sure that anchorElement',
+                    'is the part of the layout.',
+                ].join(' ')
+            )
+        }
+        return false
+    }
+
+    return true
+}
