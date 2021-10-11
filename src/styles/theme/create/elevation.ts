cc18f1d64895dceb8d39b149827f872571c8f22d
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-10-11 13:54:37 +0100

    fix elevation bug

--- src/styles/theme/create/elevation.ts
+++ src/styles/theme/create/elevation.ts
@@ -18,18 +18,18 @@ export const createElevation = (
 
     const low =
         _low ?? themeType === 'light'
-            ? 'rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;'
-            : 'rgba(0, 0, 0, 0.3) 0px 1px 3px 0px, rgba(0, 0, 0, 0.2) 0px 1px 2px 0px;'
+            ? 'rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px'
+            : 'rgba(0, 0, 0, 0.3) 0px 1px 3px 0px, rgba(0, 0, 0, 0.2) 0px 1px 2px 0px'
 
     const medium =
         _medium ?? themeType === 'light'
-            ? 'rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;'
-            : 'rgba(0, 0, 0, 0.5) 0px 4px 16px -1px, rgba(0, 0, 0, 0.3) 0px 2px 4px -1px;'
+            ? 'rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px'
+            : 'rgba(0, 0, 0, 0.5) 0px 4px 16px -1px, rgba(0, 0, 0, 0.3) 0px 2px 4px -1px'
 
     const high =
         _high ?? themeType === 'light'
-            ? 'rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;'
-            : 'rgba(0, 0, 0, 0.5) 0px 10px 25px -3px, rgba(0, 0, 0, 0.3) 0px 4px 6px -2px;'
+            ? 'rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px'
+            : 'rgba(0, 0, 0, 0.5) 0px 10px 25px -3px, rgba(0, 0, 0, 0.3) 0px 4px 6px -2px'
 
     return {
         high,
