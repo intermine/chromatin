d367b70b0f8a3e1348d6a53dd0200feddfc93698
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-10-12 20:14:25 +0100

    add some utils hooks

--- src/utils/hooks/index.ts
+++ src/utils/hooks/index.ts
@@ -1,3 +1,8 @@
 export * from './use-fork-ref'
 export * from './use-outside-click'
 export * from './use-mouse-over'
+export {
+    useDebounce,
+    useDebouncedCallback,
+    useThrottledCallback,
+} from 'use-debounce'
