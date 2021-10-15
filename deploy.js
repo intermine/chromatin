0618f3755dff7c1969985897b47ba5131d30f37a
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-10-15 12:25:34 +0100

    add deploy script

---
+++ deploy.js
@@ -0,0 +1,51 @@
+const childProcess = require('child_process')
+const { promisify } = require('util')
+const copyfiles = require('copyfiles')
+const path = require('path')
+
+const exec = promisify(childProcess.exec)
+
+// Build package
+const build = async () => {
+    const buildCommand = 'npm run prod'
+    
+    console.log(`[COMMAND]: ${buildCommand}`)
+    
+    const { stderr, stdout } = await exec(buildCommand);
+    if (stderr) {
+        throw new Error(`'${buildCommand}' failed with \n${stderr}`);
+    }
+    
+    console.log(stdout)    
+}
+
+// Copy files
+const copyImportantFiles = async () => {
+    const packageFilePath = path.join('.', 'package.json')
+    const distPath = path.join('.', 'dist')
+
+    console.log(packageFilePath, distPath)
+    await copyfiles([packageFilePath, distPath], { flat: true }, () => { console.log('Copied package.json') })
+}
+
+// Publish
+const publish = async () => {
+    const publishCommand = 'npm publish ./dist'
+    
+    console.log(`[COMMAND]: ${publishCommand}`)
+    const { stderr, stdout } = await exec(publishCommand);
+    if (stderr) {
+       console.log(`'${publishCommand}' failed with \n${stderr}`);
+    }
+    
+    console.log(stdout)    
+}
+
+// Deploy
+const deploy = async () => {
+    await build()
+    await copyImportantFiles()
+    await publish()
+}
+
+deploy()
