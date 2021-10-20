034a51a837576e3251c0b00530242f907123ae5a
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-10-20 17:20:25 +0100

    chore: remove unused imports

--- src/list/list.stories.tsx
+++ src/list/list.stories.tsx
@@ -4,7 +4,6 @@ import { List } from './list'
 import { ListItem } from '../list-item'
 import { ListHeading } from '../list-heading'
 import { Divider } from '../divider'
-import { Button } from '../button'
 import { useState } from 'react'
 import { Box } from '../box'
 
