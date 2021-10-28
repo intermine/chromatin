68b9e8ab083af283389169a2f389f5819cb708a1
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-10-28 16:57:29 +0100

    refactor theme utils

--- src/styles/theme/utils.ts
+++ src/styles/theme/utils.ts
@@ -2,17 +2,13 @@ import { Theme, ThemeColorName, ThemePaletteColor } from './create'
 import { isProdEnv } from '../../utils'
 import { isThemeColorName } from './validate'
 import { isThemePaletteColorKey } from '.'
-import { isBasicColorKey } from '..'
+import { BasicColor, isBasicColorKey } from '..'
 
-export type ColorAndKeyTuple = [
-    ThemeColorName | 'custom',
-    keyof ThemePaletteColor | 'black' | 'white',
-    (keyof ThemePaletteColor)?
-]
+export type ColorAndKeyTuple = [ThemeColorName, keyof ThemePaletteColor]
 
 export const getColorNameAndKey = (
     color: string,
-    theme?: Theme
+    options = {} as { theme: Theme; defaultKey?: string }
 ): ColorAndKeyTuple | undefined => {
     if (typeof color !== 'string') {
         if (!isProdEnv()) {
@@ -26,50 +22,15 @@ export const getColorNameAndKey = (
         return
     }
 
-    /**
-     * Default key is 'main'
-     */
-    const [colorName, key = 'main', _ = 'main'] = color.split('.')
+    const { theme, defaultKey = 'main' } = options
+
+    const [colorName, key = defaultKey] = color.split('.')
 
     if (colorName === undefined) {
         return
     }
 
-    if (colorName === 'custom') {
-        /**
-         * it should be something like custom.abc.90
-         *
-         * TODO: fix typing.
-         */
-        return [key, _] as unknown as ColorAndKeyTuple
-    }
-
-    if (color)
-        if (colorName === 'common') {
-            /**
-             * Special case for common color palette.
-             * Currently common palette has only two colors,
-             * i.e., black and white.
-             */
-            if (key === 'black' || key === 'white') {
-                return [colorName, key] as ColorAndKeyTuple
-            }
-            return
-        }
-
     if (isThemeColorName(colorName, theme) && isThemePaletteColorKey(key)) {
-        if (colorName === 'darkGrey' || colorName === 'grey') {
-            if (key === 'main') {
-                return [colorName, 50]
-            }
-            /**
-             * For grey and darkGrey only numbered index is supported.
-             */
-            if (Number.isNaN(Number(key))) {
-                return
-            }
-        }
-
         return [colorName, key]
     }
 }
@@ -90,19 +51,20 @@ export const getThemeColorUsingKey = (
         return ''
     }
 
-    const [color, key, _ = 'main'] = colorTuple
+    const [color, key] = colorTuple
 
-    if (color === 'custom') {
+    if (color.startsWith('$')) {
         try {
-            return theme.palette.custom[key][_]
+            const colorName = color.slice(1)
+            return (theme.palette.other[colorName] as any)[key]
         } catch {
             if (!isProdEnv()) {
                 console.error(
-                    '[Chromatin - getThemeColorUsingKey]: Custom color '.concat(
-                        'is not defined. theme.palette.custom.',
-                        key.toString(),
+                    '[Chromatin - getThemeColorUsingKey]: Other color '.concat(
+                        'is not defined. theme.palette.other.',
+                        color.slice(1),
                         '.',
-                        _.toString(),
+                        key.toString(),
                         ' throw error'
                     )
                 )
@@ -111,53 +73,18 @@ export const getThemeColorUsingKey = (
         }
     }
 
-    if (color === 'common') {
-        if (key === 'black' || key === 'white') {
-            return theme.palette.common[key]
+    if (color === 'grey' || color === 'darkGrey') {
+        if (isBasicColorKey(key)) {
+            return theme.palette[color][key]
         }
 
-        if (!isProdEnv()) {
-            console.error(
-                '[Chromatin - getThemeColorUsingKey]: Color is '.concat(
-                    'common. But key is not satisfying the condition. ',
-                    'It should be "black" or "white". Key: ',
-                    key.toString()
-                )
-            )
-        }
-        return ''
+        let _key: keyof BasicColor = 50
+
+        if (key === 'mainDarkShade') _key = 60
+        if (key === 'mainLightShade') _key = 40
+
+        return theme.palette[color][_key]
     }
 
-    if (key !== 'black' && key !== 'white') {
-        if (color === 'grey' || color === 'darkGrey') {
-            if (isBasicColorKey(key)) {
-                return theme.palette[color][key]
-            }
-
-            if (!isProdEnv()) {
-                console.error(
-                    '[Chromatin - getThemeColorUsingKey]: Color is '.concat(
-                        'a basic color. But key is not satisfying condition',
-                        'of basic color key. Key: ',
-                        key
-                    )
-                )
-            }
-
-            return ''
-        }
-        return theme.palette[color][key]
-    }
-
-    if (!isProdEnv()) {
-        console.error(
-            '[Chromatin - getThemeColorUsingKey]: Color is '.concat(
-                ' not common. But key is either',
-                '"black" or "white". This is not allowed. Key: ',
-                key
-            )
-        )
-    }
-
-    return ''
+    return theme.palette[color][key]
 }
