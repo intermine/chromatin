2075e8e404a10fd84d558c988cd83992dbc8bf15
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-10-28 13:18:21 +0100

    add util to find key is of basicColor type or not

--- src/styles/colors/utils.ts
+++ src/styles/colors/utils.ts
@@ -1,4 +1,24 @@
+import { BasicColor } from '.'
 import type { ThemeType } from './color'
 
 export const themeTernaryOperator = <T>(theme: ThemeType, v1: T, v2: T): T =>
     theme === 'light' ? v1 : v2
+
+export const isBasicColorKey = (
+    key: number | string
+): key is keyof BasicColor => {
+    if (
+        key == '10' ||
+        key == '20' ||
+        key == '30' ||
+        key == '40' ||
+        key == '50' ||
+        key == '60' ||
+        key == '70' ||
+        key == '80' ||
+        key == '90'
+    ) {
+        return true
+    }
+    return false
+}
